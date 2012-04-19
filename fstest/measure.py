626b8a33c079bfca965e9c9e16f90770e40ba170
lfr <lfr@2079a70e-0e9d-4454-99f1-4d4c78da5c83>
2012-04-19 14:22:42 +0000

    git-svn-id: svn+ssh://svn.open.ch/pkg-OSAGfsperf/trunk@5 2079a70e-0e9d-4454-99f1-4d4c78da5c83

--- fstest/measure.py
+++ fstest/measure.py
@@ -1,25 +1,45 @@
 #!/usr/bin/python
 
 import os, sys
+import random
+
 gluster_path = '/mnt/geo-sync2'
 drbd_path = '/mnt/drbd1'
+test_root = '/root'
 
-def start_test(self, test, size, src, dst):
+def start_test(test, count, dst, src):
 	try:
 		pid = os.fork();
 	except OSError, e:
 		raise RuntimeError("Fork failed: %s [%d]" % (e.strerror, e.errno))
 	if (pid != 0):
 		try:
-			os.execl('/root/fstest.sh', 'seqwrite', )
+			if (src != None):
+				os.execl(test_root + '/fstest.sh', test, count, dst, src)
+			else:
+				os.execl(test_root + '/fstest.sh', test, count, dst)
 		except Exception, e:
 			os._exit(255)
-	
+
+def usage():
+	print('Usage: measure.py test size (gluster|drbd) [src]')
+	sys.exit(1)
 
 if __name__ == '__main__':
 	if (len(sys.argv) < 4):
-		print('Not enough arguments')
-		sys.exit(-1)
+		usage()
 	
 	test = sys.argv[1]
+	count = sys.argv[2]
 	
+	if ((sys.argv[3] == 'gluster') or (sys.argv[3] == 'drbd')):
+		dst = gluster_path + '/data.' + str(random.random())[4:] + '.out'
+	else:
+		usage()
+
+	if (len(sys.argv) > 4):
+		src = sys.argv[4]
+	else
+		src = None
+
+	start_test(test, count, dst, src)
