177006d8cd622bbf7bd9a421bd697a7f70b0fb41
lfr <lfr@2079a70e-0e9d-4454-99f1-4d4c78da5c83>
2012-07-11 16:41:49 +0000

    Use functions from syncedfs-daemon.
    git-svn-id: svn+ssh://svn.open.ch/pkg-OSAGfsperf/trunk@74 2079a70e-0e9d-4454-99f1-4d4c78da5c83

--- analyse-log/main.c
+++ analyse-log/main.c
@@ -11,6 +11,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <ftw.h>
 #include "../syncedfs-common/protobuf/syncedfs.pb-c.h"
 #include "../syncedfs-common/logging_functions.h"
 #include "../syncedfs-common/message_functions.h"
@@ -19,79 +20,135 @@
 #include "../syncedfs-common/lib/create_pid_file.h"
 #include "../syncedfs-common/lib/uthash.h"
 #include "../syncedfs-daemon/optimization.h"
+#include "../syncedfs-daemon/client.h"
 
-/*typedef struct fileop {
-    char filename[PATH_MAX]; // key
-    int order; // files must be transfered in order
-    GenericOperation **operations;
-    int capacity;
-    int nelem;
-    UT_hash_handle hh;
-} fileop_t;*/
-
-fileop_t *files = NULL; // Hash map
-
-/*int sortByOrder(fileop_t *a, fileop_t *b) {
-    if (a->order == b->order)
-        return 0;
-    return (a->order < b->order) ? -1 : 1;
-}*/
-
-int addOperation(char *relpath, GenericOperation *genop) {
-    static int fileorder = 0;
-    static int id = 0;
+void writesSize(void) {
     fileop_t *f;
+    GenericOperation *genop; // current operation
+    long long totalsum = 0;
 
-    genop->has_id = 1;
-    genop->id = id++;
+    // first try to match inodefiles to files
+    for (f = getFiles(); f != NULL; f = (fileop_t*) (f->hh.next)) {
+        for (int i = 0; i < f->nelem; i++) {
+            genop = *(f->operations + i);
+            if (genop == NULL) {
+                continue;
+            }
 
-    HASH_FIND_STR(files, relpath, f);
-
-    // key not found
-    if (f == NULL) {
-        f = (fileop_t*) malloc(sizeof (fileop_t));
-        if (f == NULL) {
-            errMsg(LOG_ERR, "Could not allocate memory for file operations.");
-            return -1;
-        }
-
-        (void) strcpy(f->relpath, relpath);
-        f->order = fileorder++;
-        f->capacity = VECTOR_INIT_CAPACITY;
-        f->operations = malloc(VECTOR_INIT_CAPACITY * sizeof (fileop_t*));
-
-        if (f->operations == NULL) {
-            errMsg(LOG_ERR, "Could not allocate memory for file operations.");
-            return -1;
-        }
-
-        HASH_ADD_STR(files, relpath, f);
-    }
-
-    // extend vector of operations if needed
-    if (f->nelem == (f->capacity - 1)) {
-        f->capacity = f->capacity * 2;
-        f->operations = realloc(f->operations,
-                f->capacity * sizeof (fileop_t*));
-
-        if (f->operations == NULL) {
-            errMsg(LOG_ERR, "Could not allocate memory for file operations. "
-                    "Requested memory: %d", f->capacity * sizeof (fileop_t*));
-            return -1;
+            if (genop->type == GENERIC_OPERATION__TYPE__WRITE)
+                totalsum += genop->write_op->size;
         }
     }
-
-    *(f->operations + f->nelem) = genop;
-    f->nelem++;
-
-    return 0;
+    printf("Total size: %lld\n", totalsum);
 }
 
-int loadLog(int logfd) {
+void optimizedWritesSize(void) {
+    fileop_t *f;
+    GenericOperation *genop; // current operation
+    long long totalsum = 0;
+
+    // first try to match inodefiles to files
+    for (f = getFiles(); f != NULL; f = (fileop_t*) (f->hh.next)) {
+        optimizeOperations(f);
+        for (int i = 0; i < f->nelem; i++) {
+            genop = *(f->operations + i);
+            if (genop == NULL) {
+                continue;
+            }
+
+            if (genop->type == GENERIC_OPERATION__TYPE__WRITE)
+                totalsum += genop->write_op->size;
+        }
+    }
+    printf("Total size: %lld\n", totalsum);
+}
+
+void printOperation(GenericOperation *op, int i) {
+    if (op == NULL) {
+        printf("Operation %d, type: empty\n", i);
+        return;
+    }
+
+    switch (op->type) {
+        case GENERIC_OPERATION__TYPE__CREATE:
+            printf("Operation %d, type: create, mode: %d\n",
+                    i, (int) op->create_op->mode);
+            break;
+        case GENERIC_OPERATION__TYPE__MKNOD:
+            printf("Operation %d, type: mknod, mode: %d, dev: %ld\n",
+                    i, (int) op->mknod_op->mode,
+                    (long int) op->mknod_op->dev);
+            break;
+        case GENERIC_OPERATION__TYPE__MKDIR:
+            printf("Operation %d, type: mkdir, mode: %d\n",
+                    i, (int) op->mkdir_op->mode);
+            break;
+        case GENERIC_OPERATION__TYPE__SYMLINK:
+            printf("Operation %d, type: symlink, newpath: %s\n",
+                    i, op->symlink_op->newpath);
+            break;
+        case GENERIC_OPERATION__TYPE__LINK:
+            printf("Operation %d, type: link, oldpath: %s newpath: %s\n",
+                    i, op->link_op->oldpath, op->link_op->newpath);
+            break;
+        case GENERIC_OPERATION__TYPE__WRITE:
+            printf("Operation %d, type: write, offset: %ld, size: %d, id: %d\n",
+                    i, (long int) op->write_op->offset,
+                    (int) op->write_op->size, (int) op->id);
+            break;
+        case GENERIC_OPERATION__TYPE__UNLINK:
+            printf("Operation %d, type: unlink, inode: %ld\n", i, op->unlink_op->inode);
+            break;
+        case GENERIC_OPERATION__TYPE__RMDIR:
+            printf("Operation %d, type: rmdir\n", i);
+            break;
+        case GENERIC_OPERATION__TYPE__TRUNCATE:
+            printf("Operation %d, type: truncate, newsize: %d, id: %d\n",
+                    i, (int) op->truncate_op->newsize, (int) op->id);
+            break;
+        case GENERIC_OPERATION__TYPE__CHMOD:
+            printf("Operation %d, type: chmod, mode: %d\n",
+                    i, (int) op->chmod_op->mode);
+            break;
+        case GENERIC_OPERATION__TYPE__CHOWN:
+            printf("Operation %d, type: chown, uid: %d, gid: %d\n",
+                    i, (int) op->chown_op->uid,
+                    (int) op->chown_op->gid);
+            break;
+        case GENERIC_OPERATION__TYPE__RENAME:
+            printf("Operation %d, type: rename, newpath: %s\n",
+                    i, op->rename_op->newpath);
+            break;
+        case GENERIC_OPERATION__TYPE__SETXATTR:
+        case GENERIC_OPERATION__TYPE__REMOVEXATTR:
+            break;
+    }
+}
+
+void printLog(void) {
+    fileop_t *ff = getFiles();
+    HASH_SORT(ff, sortByOrder);
+    fileop_t *f;
+    GenericOperation *op;
+
+    for (f = ff; f != NULL; f = (fileop_t*) (f->hh.next)) {
+        printf("%s\n", f->relpath);
+        //qsort(f->operations, f->nelem, sizeof (GenericOperation *),cmpOperationType);
+        optimizeOperations(f);
+
+        for (int i = 0; i < f->nelem; i++) {
+            op = *(f->operations + i);
+            printOperation(op, i);
+        }
+    }
+}
+
+int printPureLog(int logfd) {
     int alocatedmem = 0;
     uint8_t *buf = NULL;
     uint32_t msglen;
     FileOperation *fileop;
+    int i = 0;
 
     while (read(logfd, &msglen, sizeof (uint32_t)) == sizeof (uint32_t)) {
         msglen = ntohl(msglen);
@@ -116,139 +173,16 @@ int loadLog(int logfd) {
         //file_operation__free_unpacked(fileop, NULL);
         fileop = file_operation__unpack(NULL, msglen, buf);
 
-        if (addOperation(fileop->relative_path, fileop->op) != 0) {
-            errMsg(LOG_ERR, "Could add operation to file operations. "
-                    "Exiting sync.");
-            free(buf);
-            return -1;
-        }
+        printf("%s: ", fileop->relative_path);
+        printOperation(fileop->op, i++);
     }
 
     free(buf);
+
+    // matchInodefiles
+    matchInodefiles();
+
     return 0;
-}
-
-void writesSize(void) {
-    fileop_t *f;
-    GenericOperation *genop; // current operation
-    long long totalsum = 0;
-
-    // first try to match inodefiles to files
-    for (f = files; f != NULL; f = (fileop_t*) (f->hh.next)) {
-        for (int i = 0; i < f->nelem; i++) {
-            genop = *(f->operations + i);
-            if (genop == NULL) {
-                continue;
-            }
-
-            if (genop->type == GENERIC_OPERATION__TYPE__WRITE)
-                totalsum += genop->write_op->size;
-        }
-    }
-    printf("Total size: %lld\n", totalsum);
-}
-
-void optimizedWritesSize(void) {
-    fileop_t *f;
-    GenericOperation *genop; // current operation
-    long long totalsum = 0;
-
-    // first try to match inodefiles to files
-    for (f = files; f != NULL; f = (fileop_t*) (f->hh.next)) {
-        optimizeOperations(f);
-        for (int i = 0; i < f->nelem; i++) {
-            genop = *(f->operations + i);
-            if (genop == NULL) {
-                continue;
-            }
-
-            if (genop->type == GENERIC_OPERATION__TYPE__WRITE)
-                totalsum += genop->write_op->size;
-        }
-    }
-    printf("Total size: %lld\n", totalsum);
-}
-
-int sortByOrder(fileop_t *a, fileop_t *b) {
-    if (a->order == b->order)
-
-        return 0;
-    return (a->order < b->order) ? -1 : 1;
-}
-
-void printLog(void) {
-    HASH_SORT(files, sortByOrder);
-    fileop_t *f;
-    GenericOperation *op;
-
-    for (f = files; f != NULL; f = (fileop_t*) (f->hh.next)) {
-        printf("%s\n", f->relpath);
-        //qsort(f->operations, f->nelem, sizeof (GenericOperation *),cmpOperationType);
-        optimizeOperations(f);
-
-        for (int i = 0; i < f->nelem; i++) {
-            op = *(f->operations + i);
-            if (op == NULL) {
-                printf("Operation %d, type: empty\n", i);
-                continue;
-            }
-
-            switch (op->type) {
-                case GENERIC_OPERATION__TYPE__CREATE:
-                    printf("Operation %d, type: create, mode: %d\n",
-                            i, (int) op->create_op->mode);
-                    break;
-                case GENERIC_OPERATION__TYPE__MKNOD:
-                    printf("Operation %d, type: mknod, mode: %d, dev: %ld\n",
-                            i, (int) op->mknod_op->mode,
-                            (long int) op->mknod_op->dev);
-                    break;
-                case GENERIC_OPERATION__TYPE__MKDIR:
-                    printf("Operation %d, type: mkdir, mode: %d\n",
-                            i, (int) op->mkdir_op->mode);
-                    break;
-                case GENERIC_OPERATION__TYPE__SYMLINK:
-                    printf("Operation %d, type: symlink, newpath: %s\n",
-                            i, op->symlink_op->newpath);
-                    break;
-                case GENERIC_OPERATION__TYPE__LINK:
-                    printf("Operation %d, type: link, newpath: %s\n",
-                            i, op->link_op->newpath);
-                    break;
-                case GENERIC_OPERATION__TYPE__WRITE:
-                    printf("Operation %d, type: write, offset: %ld, size: %d, id: %d\n",
-                            i, (long int) op->write_op->offset,
-                            (int) op->write_op->size, (int) op->id);
-                    break;
-                case GENERIC_OPERATION__TYPE__UNLINK:
-                    printf("Operation %d, type: unlink\n", i);
-                    break;
-                case GENERIC_OPERATION__TYPE__RMDIR:
-                    printf("Operation %d, type: rmdir\n", i);
-                    break;
-                case GENERIC_OPERATION__TYPE__TRUNCATE:
-                    printf("Operation %d, type: truncate, newsize: %d, id: %d\n",
-                            i, (int) op->truncate_op->newsize, (int) op->id);
-                    break;
-                case GENERIC_OPERATION__TYPE__CHMOD:
-                    printf("Operation %d, type: chmod, mode: %d\n",
-                            i, (int) op->chmod_op->mode);
-                    break;
-                case GENERIC_OPERATION__TYPE__CHOWN:
-                    printf("Operation %d, type: chown, uid: %d, gid: %d\n",
-                            i, (int) op->chown_op->uid,
-                            (int) op->chown_op->gid);
-                    break;
-                case GENERIC_OPERATION__TYPE__RENAME:
-                    printf("Operation %d, type: rename, newpath: %s\n",
-                            i, op->rename_op->newpath);
-                    break;
-                case GENERIC_OPERATION__TYPE__SETXATTR:
-                case GENERIC_OPERATION__TYPE__REMOVEXATTR:
-                    break;
-            }
-        }
-    }
 }
 
 /*
@@ -267,6 +201,16 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Could open log file.");
         return (EXIT_FAILURE);
     }
+
+    if (argc == 3 && strcmp(argv[2], "pure") == 0) {
+        if (printPureLog(logfd) != 0) {
+            fprintf(stderr, "Could not load log.");
+            return -1;
+        }
+        return 0;
+    }
+    
+    strcpy(config.snapshot, "/media/btrfs/syncedfs/primary/physical");
 
     if (loadLog(logfd) != 0) {
         fprintf(stderr, "Could not load log.");
--- analyse-log/nbproject/Makefile-Debug.mk
+++ analyse-log/nbproject/Makefile-Debug.mk
@@ -36,16 +36,21 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 OBJECTFILES= \
 	${OBJECTDIR}/_ext/1275677342/message_functions.o \
 	${OBJECTDIR}/_ext/1275677342/path_functions.o \
+	${OBJECTDIR}/_ext/1260215813/server.o \
 	${OBJECTDIR}/_ext/988793768/region_locking.o \
 	${OBJECTDIR}/_ext/1275677342/config_functions.o \
+	${OBJECTDIR}/_ext/1275677342/syncid.o \
 	${OBJECTDIR}/_ext/988793768/get_num.o \
-	${OBJECTDIR}/_ext/988793768/become_daemon.o \
 	${OBJECTDIR}/_ext/813881960/syncedfs.pb-c.o \
+	${OBJECTDIR}/_ext/988793768/become_daemon.o \
 	${OBJECTDIR}/_ext/988793768/inet_sockets.o \
 	${OBJECTDIR}/_ext/988793768/alt_functions.o \
+	${OBJECTDIR}/_ext/1260215813/client.o \
 	${OBJECTDIR}/_ext/988793768/create_pid_file.o \
+	${OBJECTDIR}/_ext/1260215813/config.o \
 	${OBJECTDIR}/main.o \
 	${OBJECTDIR}/_ext/1275677342/logging_functions.o \
+	${OBJECTDIR}/_ext/1260215813/snapshot.o \
 	${OBJECTDIR}/_ext/1260215813/optimization.o
 
 
@@ -83,6 +88,11 @@ ${OBJECTDIR}/_ext/1275677342/path_functions.o: ../syncedfs-common/path_functions
 	${RM} $@.d
 	$(COMPILE.c) -g `pkg-config --cflags libprotobuf-c` `pkg-config --cflags libconfig`    -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1275677342/path_functions.o ../syncedfs-common/path_functions.c
 
+${OBJECTDIR}/_ext/1260215813/server.o: ../syncedfs-daemon/server.c 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1260215813
+	${RM} $@.d
+	$(COMPILE.c) -g `pkg-config --cflags libprotobuf-c` `pkg-config --cflags libconfig`    -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1260215813/server.o ../syncedfs-daemon/server.c
+
 ${OBJECTDIR}/_ext/988793768/region_locking.o: ../syncedfs-common/lib/region_locking.c 
 	${MKDIR} -p ${OBJECTDIR}/_ext/988793768
 	${RM} $@.d
@@ -93,20 +103,25 @@ ${OBJECTDIR}/_ext/1275677342/config_functions.o: ../syncedfs-common/config_funct
 	${RM} $@.d
 	$(COMPILE.c) -g `pkg-config --cflags libprotobuf-c` `pkg-config --cflags libconfig`    -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1275677342/config_functions.o ../syncedfs-common/config_functions.c
 
+${OBJECTDIR}/_ext/1275677342/syncid.o: ../syncedfs-common/syncid.c 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1275677342
+	${RM} $@.d
+	$(COMPILE.c) -g `pkg-config --cflags libprotobuf-c` `pkg-config --cflags libconfig`    -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1275677342/syncid.o ../syncedfs-common/syncid.c
+
 ${OBJECTDIR}/_ext/988793768/get_num.o: ../syncedfs-common/lib/get_num.c 
 	${MKDIR} -p ${OBJECTDIR}/_ext/988793768
 	${RM} $@.d
 	$(COMPILE.c) -g `pkg-config --cflags libprotobuf-c` `pkg-config --cflags libconfig`    -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/988793768/get_num.o ../syncedfs-common/lib/get_num.c
 
-${OBJECTDIR}/_ext/988793768/become_daemon.o: ../syncedfs-common/lib/become_daemon.c 
-	${MKDIR} -p ${OBJECTDIR}/_ext/988793768
-	${RM} $@.d
-	$(COMPILE.c) -g `pkg-config --cflags libprotobuf-c` `pkg-config --cflags libconfig`    -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/988793768/become_daemon.o ../syncedfs-common/lib/become_daemon.c
-
 ${OBJECTDIR}/_ext/813881960/syncedfs.pb-c.o: ../syncedfs-common/protobuf/syncedfs.pb-c.c 
 	${MKDIR} -p ${OBJECTDIR}/_ext/813881960
 	${RM} $@.d
 	$(COMPILE.c) -g `pkg-config --cflags libprotobuf-c` `pkg-config --cflags libconfig`    -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/813881960/syncedfs.pb-c.o ../syncedfs-common/protobuf/syncedfs.pb-c.c
+
+${OBJECTDIR}/_ext/988793768/become_daemon.o: ../syncedfs-common/lib/become_daemon.c 
+	${MKDIR} -p ${OBJECTDIR}/_ext/988793768
+	${RM} $@.d
+	$(COMPILE.c) -g `pkg-config --cflags libprotobuf-c` `pkg-config --cflags libconfig`    -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/988793768/become_daemon.o ../syncedfs-common/lib/become_daemon.c
 
 ${OBJECTDIR}/_ext/988793768/inet_sockets.o: ../syncedfs-common/lib/inet_sockets.c 
 	${MKDIR} -p ${OBJECTDIR}/_ext/988793768
@@ -118,10 +133,20 @@ ${OBJECTDIR}/_ext/988793768/alt_functions.o: ../syncedfs-common/lib/alt_function
 	${RM} $@.d
 	$(COMPILE.c) -g `pkg-config --cflags libprotobuf-c` `pkg-config --cflags libconfig`    -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/988793768/alt_functions.o ../syncedfs-common/lib/alt_functions.c
 
+${OBJECTDIR}/_ext/1260215813/client.o: ../syncedfs-daemon/client.c 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1260215813
+	${RM} $@.d
+	$(COMPILE.c) -g `pkg-config --cflags libprotobuf-c` `pkg-config --cflags libconfig`    -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1260215813/client.o ../syncedfs-daemon/client.c
+
 ${OBJECTDIR}/_ext/988793768/create_pid_file.o: ../syncedfs-common/lib/create_pid_file.c 
 	${MKDIR} -p ${OBJECTDIR}/_ext/988793768
 	${RM} $@.d
 	$(COMPILE.c) -g `pkg-config --cflags libprotobuf-c` `pkg-config --cflags libconfig`    -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/988793768/create_pid_file.o ../syncedfs-common/lib/create_pid_file.c
+
+${OBJECTDIR}/_ext/1260215813/config.o: ../syncedfs-daemon/config.c 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1260215813
+	${RM} $@.d
+	$(COMPILE.c) -g `pkg-config --cflags libprotobuf-c` `pkg-config --cflags libconfig`    -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1260215813/config.o ../syncedfs-daemon/config.c
 
 ${OBJECTDIR}/main.o: main.c 
 	${MKDIR} -p ${OBJECTDIR}
@@ -132,6 +157,11 @@ ${OBJECTDIR}/_ext/1275677342/logging_functions.o: ../syncedfs-common/logging_fun
 	${MKDIR} -p ${OBJECTDIR}/_ext/1275677342
 	${RM} $@.d
 	$(COMPILE.c) -g `pkg-config --cflags libprotobuf-c` `pkg-config --cflags libconfig`    -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1275677342/logging_functions.o ../syncedfs-common/logging_functions.c
+
+${OBJECTDIR}/_ext/1260215813/snapshot.o: ../syncedfs-daemon/snapshot.c 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1260215813
+	${RM} $@.d
+	$(COMPILE.c) -g `pkg-config --cflags libprotobuf-c` `pkg-config --cflags libconfig`    -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1260215813/snapshot.o ../syncedfs-daemon/snapshot.c
 
 ${OBJECTDIR}/_ext/1260215813/optimization.o: ../syncedfs-daemon/optimization.c 
 	${MKDIR} -p ${OBJECTDIR}/_ext/1260215813
