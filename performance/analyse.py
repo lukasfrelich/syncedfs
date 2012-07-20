66254a5bd9346d8f02f6e212e45189a33e12405f
lfr <lfr@2079a70e-0e9d-4454-99f1-4d4c78da5c83>
2012-07-20 15:12:41 +0000

    measurements scripts 1.0 (no units normalization and averages)
    git-svn-id: svn+ssh://svn.open.ch/pkg-OSAGfsperf/trunk@78 2079a70e-0e9d-4454-99f1-4d4c78da5c83

---
+++ performance/analyse.py
@@ -0,0 +1,265 @@
+#!/usr/bin/python
+import os
+import re
+import sys
+import cPickle as pickle
+import glob
+
+a_dev_stats_rev = {'0': 'rcompleted', '1': 'rmerged', '2': 'rsectors', '3': 'rtime',
+             '4': 'wcompleted', '5': 'wmerged', '6': 'wsectors',
+             '7': 'wtime', '8': 'inprogress',
+             '9': 'iotime', '10': 'iowtime'}
+
+# Achtung: 0 is pid
+a_proc_stats_rev = {'1': 'rchar', '2': 'wchar', '3': 'syscr', '4': 'syscw',
+              '5': 'rbytes', '6': 'wbytes', '7': 'cancelled_write_bytes'}
+
+a_eth_stats_rev = {'0': 'rbytes', '1': 'rpackets',  # important
+             '2': 'rerrs', '3': 'rdrop', '4': 'rfifo', '5': 'rframe',
+             '6': 'rcompressed', '7': 'rmulticast',
+             '8': 'tbytes', '9': 'tpackets',  # important
+             '10': 'terrs', '11': 'tdrop', '12': 'tfifo', '13': 'tcolls',
+             '14': 'tcarrier', '15': 'tcompressed'}
+
+
+a_dev_stats = {'rcompleted': '0', 'rmerged': '1', 'rsectors': '2', 'rtime': '3',
+             'wcompleted': '4', 'wmerged': '5', 'wsectors': '6',
+             'wtime': '7', 'inprogress': '8',
+             'iotime': '9', 'iowtime': '10'}
+
+# Achtung: 0 is pid
+a_proc_stats = {'rchar': '1', 'wchar': '2', 'syscr': '3', 'syscw': '4',
+              'rbytes': '5', 'wbytes': '6', 'cancelled_write_bytes': '7'}
+
+a_eth_stats = {'rbytes': '0', 'rpackets': '1',  # important
+             'rerrs': '2', 'rdrop': '3', 'rfifo': '4', 'rframe': '5',
+             'rcompressed': '6', 'rmulticast': '7',
+             'tbytes': '8', 'tpackets': '9',  # important
+             'terrs': '10', 'tdrop': '11', 'tfifo': '12', 'tcolls': '13',
+             'tcarrier': '14', 'tcompressed': '15'}
+
+dev_stats = []
+devs = []
+proc_stats = []
+procs = []
+eth_stats = []
+eths = []
+
+def print_computed_stats(computed_stats):
+    def strip_paths(paths):
+        return [os.path.split(path)[1] for path in paths]
+
+    def get_header_part(entities, stats, stats_rev_dict):
+        header_part = ''
+        for entity in entities:
+            for stat in stats:
+                header_part += entity + '-' + stats_rev_dict[str(stat)] + ','
+
+        return header_part
+
+    # print header
+    header = 'time,'
+    header += get_header_part(devs, dev_stats, a_dev_stats_rev)
+    header += get_header_part(strip_paths(procs), proc_stats, a_proc_stats_rev)
+    header += get_header_part(eths, eth_stats, a_eth_stats_rev)
+    print(header[:-1])
+
+    def get_stats_part(stats):
+        stats_part = ''
+        for dev in stats:
+            for stat in dev:
+                stats_part += str(round(stat, 2)) + ','
+
+        return stats_part
+
+    # print records
+    for rec in computed_stats:
+        line = str(round(rec[0], 2)) + ',' # time
+        for i in range(1, len(rec)):
+            line += get_stats_part(rec[i])
+
+        print(line[:-1])
+
+
+def analyse(stats, rates):
+    print(len(stats))
+    sdevs = set()
+    sprocs = set()
+    seths = set()
+
+    # get all values first
+    for rec in stats:
+        sdevs.update(rec[1].keys())
+        sprocs.update(rec[2].keys())
+        seths.update(rec[3].keys())
+
+    global devs
+    global procs
+    global eths
+    devs = list(sdevs)
+    procs = list(sprocs)
+    eths = list(seths)
+
+    def initialize_base_dict(x, y):
+        out = dict()
+        for i in range(x):
+            out[i] = dict()
+            for j in range(y):
+                out[i][j] = 0
+        return out
+
+    base_dev = initialize_base_dict(len(devs), len(dev_stats))
+    base_proc = initialize_base_dict(len(procs), len(proc_stats))
+    base_eth = initialize_base_dict(len(eths), len(eth_stats))
+
+    # get start time
+    if len(stats) > 0:
+        last_time = stats[0][0]     # for time_span
+        first_time = stats[0][0]    # for out_rec
+
+    computed_stats = []
+
+    if rates:
+        for rec in stats:
+            time_span = rec[0] - last_time
+            last_time = rec[0]
+
+            out_rec = [last_time - first_time]
+            if len(dev_stats) > 0:
+                out_rec.append(process_record(rec[1], devs, dev_stats, base_dev, time_span))
+            if len(proc_stats) > 0:
+                out_rec.append(process_record(rec[2], procs, proc_stats, base_proc, time_span))
+            if len(eth_stats) > 0:
+                out_rec.append(process_record(rec[3], eths, eth_stats, base_eth, time_span))
+
+            computed_stats.append(out_rec)
+    else:
+        for rec in stats:
+            time_span = rec[0] - last_time
+            last_time = rec[0]
+
+            out_rec = [last_time - first_time]
+            if len(dev_stats) > 0:
+                out_rec.append(process_record_dummy(rec[1], devs, dev_stats))
+            if len(proc_stats) > 0:
+                out_rec.append(process_record_dummy(rec[2], procs, proc_stats))
+            if len(eth_stats) > 0:
+                out_rec.append(process_record_dummy(rec[3], eths, eth_stats))
+
+            computed_stats.append(out_rec)
+
+    return computed_stats
+
+def process_record(record_dict, entities, stats, base, time_span):
+    out = []
+    i = 0
+    for entity in entities:     # sda1, sda2 or eth0, eth2...
+        j = 0
+        out_sub = []
+        for stat in stats:      # wsectors, wtime  or rbytes, wbytes
+            # if last time span was 0 second, it must have been the first one
+            # only set the base
+            if time_span == 0:
+                try:
+                    base[i][j] = record_dict[entity][stat]
+                except KeyError:
+                    pass
+                out_sub.append(0)
+            else:
+                try:
+                    # otherwise try to compute the rate
+                    out_sub.append((record_dict[entity][stat] - base[i][j]) / time_span)
+                    base[i][j] = record_dict[entity][stat]
+                except KeyError:
+                    # stat is always there, only entity might be missing
+                    # assign -1 as result, to indicate, that the entity was missing
+                    out_sub.append(-1)
+                    # and reset base
+                    base[i][j] = 0
+            j += 1
+        i += 1
+        out.append(out_sub)
+
+    return out
+
+def process_record_dummy(record_dict, entities, stats):
+    out = []
+    for entity in entities:     # sda1, sda2 or eth0, eth2...
+        out_sub = []
+        for stat in stats:      # wsectors, wtime  or rbytes, wbytes
+                try:
+                    out_sub.append(record_dict[entity][stat])
+                except Exception:
+                    out_sub.append(-1)
+        out.append(out_sub)
+
+    return out
+
+def usage():
+    def print_keys(spaces, dict):
+        # hard-coded to three columns
+        keys = sorted(dict.keys())
+        if (len(keys) % 3) != 0:
+            for i in range(3 - (len(keys) % 3)):
+                keys.append('')
+        for i in range(0, len(keys), 3):
+            print(' ' * spaces +
+                  '{0} {1} {2}'.format(keys[i], keys[i + 1], keys[i + 2]))
+        print('')
+
+    print('Usage: analyse.py [log-file] ' +
+          'dev-stats-list proc-stats-list eth-stats-list')
+
+    print('       log-file: if not specified' +
+          'last log file will be sought for')
+    print('       comma-separated list or - for no stats')
+
+    print('       dev-stats-list:')
+    print_keys(23, a_dev_stats)
+    print('       proc-stats-list:')
+    print_keys(24, a_proc_stats)
+    print('       eth-stats-list:')
+    print_keys(23, a_eth_stats)
+
+    sys.exit(1)
+
+if __name__ == '__main__':
+    # check number of command-line parameters, get log name
+    if len(sys.argv) == 5:
+        stat_lists = sys.argv[2:]
+        filename = sys.argv[1]
+    elif len(sys.argv) == 4:
+        stat_lists = sys.argv[1:]
+        filename = 'stats.log'
+        # if filename exists, create new file 'stats.log.x', where x is the
+        # lowest unused number
+        pattern = re.compile('\.([0-9]+)$')
+        maxnum = 0
+        for path in glob.glob(filename + '*'):
+            number = 0
+            if pattern.search(path):
+                number = int(pattern.search(path).group(1))
+
+            if number > maxnum:
+                maxnum = number
+
+        if maxnum > 0:
+            filename += '.' + str(maxnum)
+    else:
+        usage()
+
+    def parse_csv(csv):
+        if csv != '-':
+            return csv.split(',')
+        else:
+            return []
+
+    # get command-line options translated to numbers
+    dev_stats = [int(a_dev_stats[stat]) for stat in parse_csv(stat_lists[0])]
+    proc_stats = [int(a_proc_stats[stat]) for stat in parse_csv(stat_lists[1])]
+    eth_stats = [int(a_eth_stats[stat]) for stat in parse_csv(stat_lists[2])]
+
+    stats = pickle.load(open(filename, 'rb'))
+
+    # TODO: only supports computing rates now
+    print_computed_stats(analyse(stats, True));
--- performance/measure.py
+++ performance/measure.py
@@ -1,45 +1,194 @@
 #!/usr/bin/python
 
 import os, sys
-import random
+import re
+import time, signal
+import ctypes, os
+import glob
+import cPickle as pickle
+import timeit
 
-gluster_path = '/mnt/geo-sync2'
-drbd_path = '/mnt/drbd1'
-test_root = '/root'
+# clock_gettime is not directly available
+# source: http://stackoverflow.com/questions/1205722/how-do-i-get-monotonic-time-durations-in-python
+CLOCK_MONOTONIC = 1 # see <linux/time.h>
 
-def start_test(test, count, dst, src):
-	try:
-		pid = os.fork();
-	except OSError, e:
-		raise RuntimeError("Fork failed: %s [%d]" % (e.strerror, e.errno))
-	if (pid != 0):
-		try:
-			if (src != None):
-				os.execl(test_root + '/fstest.sh', test, count, dst, src)
-			else:
-				os.execl(test_root + '/fstest.sh', test, count, dst)
-		except Exception, e:
-			os._exit(255)
+class timespec(ctypes.Structure):
+    _fields_ = [
+        ('tv_sec', ctypes.c_long),
+        ('tv_nsec', ctypes.c_long)
+    ]
+
+librt = ctypes.CDLL('librt.so.1', use_errno=True)
+clock_gettime = librt.clock_gettime
+clock_gettime.argtypes = [ctypes.c_int, ctypes.POINTER(timespec)]
+
+def monotonic_time():
+    t = timespec()
+    if clock_gettime(CLOCK_MONOTONIC, ctypes.pointer(t)) != 0:
+        errno_ = ctypes.get_errno()
+        raise OSError(errno_, os.strerror(errno_))
+    return t.tv_sec + t.tv_nsec * 1e-9
+
+block_devices = []
+executables = []
+eth_devices = []
+
+stats = []  # all gathered statistics
+
+def start_test(testcmd):
+    try:
+        pid = os.fork();
+    except OSError, e:
+        raise RuntimeError('Fork failed: %s [%d]' % (e.strerror, e.errno))
+    if pid != 0:
+        try:
+            os.system(testcmd)
+        except Exception:
+            os._exit(255)
+
+
+def start_measurements():
+    signal.signal(signal.SIGALRM, gather_stats)
+    signal.signal(signal.SIGINT, end_measurements)
+    signal.setitimer(signal.ITIMER_REAL, 0.1, 0.1)
+
+
+def end_measurements(signum, frame):
+    signal.setitimer(signal.ITIMER_REAL, 0, 0)
+
+    # dump stats
+    filename = 'stats.log'
+
+    if os.path.exists(filename):
+        # if filename exists, create new file 'stats.log.x', where x is the
+        # lowest unused number
+        pattern = re.compile('\.([0-9]+)$')
+        maxnum = 0
+        for path in glob.glob(filename + '*'):
+            number = 0
+            if (pattern.search(path)):
+                number = int(pattern.search(path).group(1))
+
+            if number > maxnum:
+                maxnum = number
+        filename += '.' + str(maxnum + 1);
+
+    # if saving fails, try home directory
+    try:
+        pickle.dump(stats, open(filename, 'wb'))
+    except Exception:
+        filename = os.path.expanduser('~/stats.log')
+        pickle.dump(stats, open(filename, 'wb'))
+    sys.exit()
+
+
+def gather_stats(signum, frame):
+    stats.append([monotonic_time(), block_dev_io_stats(), process_io_stats(),
+                  net_stats()])
+
+
+def block_dev_io_stats():
+    filtered_devs = {}
+    with open('/proc/diskstats', 'r') as f:
+        for line in f:
+            fields = line.split()
+            if filter_equal(fields[2], block_devices):
+                filtered_devs[fields[2]] = [long(e) for e in fields[3:]]
+
+    return filtered_devs
+
+
+def process_io_stats():
+    filtered_procs = {}
+    for path in glob.glob('/proc/[1-9]*'):
+        try:
+            f = open(path + '/cmdline', 'r')
+            filename = (f.read().split('\x00'))[0]
+            f.close()
+            if filter_endswith(filename, executables):
+                iostats = [path[6:]]    # pid
+
+                f = open(path + '/io', 'r')
+                for line in f:
+                    if len(line) < 1:
+                        continue
+                    iostats.append(long((line.split())[1]))
+                f.close()
+
+                # if we get multiple processes with same name, sum-up the stats
+                if filename in filtered_procs:
+                    oldiostats = filtered_procs[filename]
+                    for i in range(1, 7):
+                        iostats[i] += oldiostats[i]
+                filtered_procs[filename] = iostats
+        except IOError:    # process might have disappear in the meantime
+            pass
+
+    return filtered_procs
+
+
+def net_stats():
+    filtered_devs = {}
+    with open('/proc/net/dev', 'r') as f:
+        for line in f:
+            fields = line.split()
+            if filter_startswith(fields[0], eth_devices):
+                # fields[:-1] because, there is always trailing :
+                filtered_devs[fields[0][:-1]] = [long(e) for e in fields[1:]]
+
+    return filtered_devs
+
+
+def filter_startswith(line, keywords):
+    for key in keywords:
+        if line.startswith(key):
+            return True
+    return False
+
+
+def filter_endswith(line, keywords):
+    for key in keywords:
+        if line.endswith(key):
+            return True
+    return False
+
+
+def filter_equal(line, keywords):
+    for key in keywords:
+        if key == line:
+            return True
+    return False
+
 
 def usage():
-	print('Usage: measure.py test size (gluster|drbd) [src]')
-	sys.exit(1)
+    print('Usage: measure.py test-command devs procs eths')
+    print('       test-command will not by interpreted by this script')
+    print('       comma-separated list or - for no stats')
+    sys.exit(1)
 
 if __name__ == '__main__':
-	if (len(sys.argv) < 4):
-		usage()
-	
-	test = sys.argv[1]
-	count = sys.argv[2]
-	
-	if ((sys.argv[3] == 'gluster') or (sys.argv[3] == 'drbd')):
-		dst = gluster_path + '/data.' + str(random.random())[4:] + '.out'
-	else:
-		usage()
+#	t = timeit.Timer("gather_stats(None, None)",
+#        "from __main__ import gather_stats")
+#	print t.timeit(10)
 
-	if (len(sys.argv) > 4):
-		src = sys.argv[4]
-	else
-		src = None
+    if (len(sys.argv) < 5):
+        usage()
 
-	start_test(test, count, dst, src)
+    entities_lists = sys.argv[2:]
+
+    def parse_csv(csv):
+        if csv != '-':
+            return csv.split(',')
+        else:
+            return []
+
+    # get command-line options translated to numbers
+    block_devices = parse_csv(entities_lists[0])
+    executables = parse_csv(entities_lists[1])
+    eth_devices = parse_csv(entities_lists[2])
+
+    testcmd = sys.argv[1]
+    start_measurements()
+    while True:
+        time.sleep(5);
+    start_test()
